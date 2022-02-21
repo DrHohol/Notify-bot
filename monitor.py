94ed30ce7ed1f7255c4e061fa2e8ab24ffd5995f
DrHohol <zencraft23@gmail.com>
2022-02-21 08:57:33 +0200

    starter

---
+++ monitor.py
@@ -0,0 +1,18 @@
+import asyncio
+from bot import bot
+from controller import Controller
+from time import sleep
+
+async def send_notify():
+    while True:
+        notifies = Controller.get_active()
+        if notifies:
+            for notify in notifies:
+                await bot.send_message(
+                    notify["chat_id"],
+                    text=notify["text"],
+                    reply_to_message_id=notify["reply_id"],
+                )
+        sleep(10)
+
+asyncio.run(send_notify())
